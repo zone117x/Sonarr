5514f199529ad9ee00da0291c1114f3acf7a0fc8
Mark McDowall <markus.mcd5@gmail.com>
2015-07-17 17:53:58 -0700

    moar fixes

--- src/UI/Shared/Grid/SonarrPageableCollection.js
+++ src/UI/Shared/Grid/SonarrPageableCollection.js
@@ -14,24 +14,25 @@ var SonarrPageableCollection = PageableCollection.extend({
         if (event === 'reset') {
             var currentPage = this.state.currentPage;
             var pageSize = this.state.pageSize;
-            var pageStart = currentPage * pageSize;
-            var pageEnd = pageStart + pageSize;
 
             originalEventHandler.apply(this, [].slice.call(arguments, 1));
 
             var totalPages = Math.ceil(this.state.totalRecords / pageSize);
+            var newPage = Math.min(currentPage, totalPages);
 
-            if (currentPage > totalPages) {
-                this.pageCollection.reset(this.fullCollection.models.slice(0, pageSize),
-                    _.extend({}, options, {parse: false}));
-            } else if (currentPage !== this.state.currentPage) {
-                this.state.currentPage = currentPage;
+            if (newPage !== this.state.currentPage) {
+                this.state.currentPage = newPage;
 
                 // If backbone pageable fixes their reset bug
                 // (they reset the page number, but not the range),
-                // we'll need to do this
-//                this.pageCollection.reset(this.fullCollection.models.slice(pageStart, pageEnd),
-//                    _.extend({}, options, {parse: false}));
+                // we'll want to do this for all resets where the page number changed
+                if (currentPage !== newPage) {
+                    var pageStart = (newPage - 1) * pageSize;
+                    var pageEnd = pageStart + pageSize;
+
+                    this.pageCollection.reset(this.fullCollection.models.slice(pageStart, pageEnd),
+                        _.extend({}, options, { parse : false }));
+                }
             }
         } else {
             originalEventHandler.call(this, [].slice.call(arguments, 1));
